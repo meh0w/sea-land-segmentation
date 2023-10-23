65a354a6eb28790840de0649ac433785a92c6586
meh0w <michal.gruba00@gmail.com>
2023-10-23 02:01:04 +0200

    Fixes in data generator

--- generator.py
+++ generator.py
@@ -10,11 +10,11 @@ class DataLoader(keras.utils.all_utils.Sequence):
         self.labels = labels
         self.batch_size = batch_size
 
-        self.input_size = (np.moveaxis(np.load(image_filenames[0]), -1, 0)[:,:,:]/24_000).shape
+        self.input_size = (np.moveaxis(np.load(image_filenames[0]), 0, 0)[:,:,:]).shape
 
 
     def __len__(self) :
-        return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
+        return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(int)
 
 
     def __getitem__(self, idx) :
@@ -24,6 +24,6 @@ class DataLoader(keras.utils.all_utils.Sequence):
         images = []
         labels = []
         for img_file, label_file in zip(batch_x, batch_y):
-            images.append(np.moveaxis(np.load(img_file), -1, 0)[:,:,:]/24_000)
-            labels.append(to_sparse(np.load(label_file)[0]))
+            images.append(np.moveaxis(np.load(img_file), 0, 0)[:,:,:]/22_000) #24_000
+            labels.append(np.moveaxis(to_sparse(np.load(label_file)[0]),0,-1))
         return np.asarray(images), np.asarray(labels)
