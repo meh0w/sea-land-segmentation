32ad3c9ecf59bd2722598135b407f83acf0a2ea2
meh0w <michal.gruba00@gmail.com>
2023-10-23 02:03:58 +0200

    Added metrics callback, wandb and fixes

--- main.py
+++ main.py
@@ -1,31 +1,98 @@
 import numpy as np
-from DeepUNet import get_model
+from SeNet import get_model
 import os
 import matplotlib.pyplot as plt
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import CategoricalCrossentropy
 import sklearn
 from generator import DataLoader
 from utils import get_file_names, to_sparse
+from tensorflow.keras.layers import Softmax
+from metrics import IoU, dice_coeff
+import wandb
+from keras.callbacks import Callback
 
 PATH = rf'.\sample\train'
+EPOCHS = 50
 BATCH_SIZE = 15
+LEARNING_RATE = 0.000001
+TRAIN_PART = 0.7
+
+class MetricsCallback(Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        res = self.model.predict(x=data_loader_valid)
+
+        res_smax = Softmax(axis=-1)(res)
+
+        pred = np.argmax(res_smax, 3)
+        real = np.argmax(labs_valid, 1)
+        iou_ = IoU(pred, real)
+        dice_ = dice_coeff(pred, real)
+        
+        wandb.log(
+            {
+            f'IoU mean': np.mean(iou_),
+            f'IoU land': iou_[0],
+            f'IoU water': iou_[1],
+            f'Dice mean': np.mean(dice_),
+            f'Dice land': dice_[0],
+            f'Dice water': dice_[1]
+             }
+            )
+        
+        if np.mean(iou_) > 0.95:
+            self.model.save_weights(f'./weights/{np.mean(iou_):.4f}.h5')
+            self.model.stop_training = True
+
+
+# # start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="Masters thesis",
+    
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": LEARNING_RATE,
+    "architecture": "SeNet",
+    "dataset": "SWED",
+    "epochs": EPOCHS,
+    "batch_size": BATCH_SIZE
+    }
+)
+
 
 img_files, label_files = get_file_names(PATH)
+idx = np.random.choice(np.arange(len(img_files)), int(np.floor(len(img_files)*0.7)), replace=False)
 
-data_loader = DataLoader(img_files, label_files, BATCH_SIZE)
+data_loader_train = DataLoader(img_files[idx], label_files[idx], BATCH_SIZE)
+data_loader_valid = DataLoader(np.delete(img_files, idx), np.delete(label_files, idx), BATCH_SIZE)
 
-m = get_model(data_loader.input_size)
+m = get_model(data_loader_train.input_size, BATCH_SIZE)
 
-opt = Adam(learning_rate=0.0001, clipnorm=1.0, clipvalue=0.5)
-m.compile(optimizer=opt, loss="categorical_crossentropy", metrics=['accuracy'])
-m.fit(x=data_loader, epochs=5)
+metrics_callback = MetricsCallback()
+loss = CategoricalCrossentropy(from_logits=True)
+opt = Adam(learning_rate=LEARNING_RATE)
+labs_train = np.asarray([to_sparse(np.load(label)[0]) for label in data_loader_train.labels])
+labs_valid = np.asarray([to_sparse(np.load(label)[0]) for label in data_loader_valid.labels])
 
-res = m.predict(x=data_loader)
-labs = [to_sparse(np.load(label)[0]) for label in data_loader.labels]
+m.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+m.fit(x=data_loader_train, validation_data=data_loader_valid ,epochs=EPOCHS, callbacks=[metrics_callback])
 
-fig, ax = plt.subplots(5)
-for i, im in enumerate(res):
-    ax[i].imshow(np.argmax(im, 0))
+res = m.predict(x=data_loader_train)
+
+res_smax = Softmax(axis=-1)(res)
+
+predicted = np.argmax(res_smax, 3)
+real = np.argmax(labs_train, 1)
+
+fig, ax = plt.subplots(5, 2)
+for i, im in enumerate(predicted):
+    ax[i, 0].imshow(im)
+    ax[i, 1].imshow(real[i])
     if i == 4:
         break
+
+iou = np.mean(IoU(predicted, real))
+dice = np.mean(dice_coeff(predicted, real))
 plt.show()
+print('a')
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import os
-# from tifffile import tifffile
+from tifffile import tifffile
 
 def to_sparse(im):
     sparse_im = np.zeros((2,)+im.shape)
@@ -17,24 +17,24 @@ def load_data(path, part):
     labs = []
 
     if part == 'test':
-        for file in os.listdir(rf'{path}\images'):
+        for file in os.listdir(rf'{path}/images'):
             if file.endswith('.tif'):
-                ims.append(tifffile.imread(rf'{path}\images\{file}')[:3,:,:])
+                ims.append(tifffile.imread(rf'{path}/images/{file}')[:,:,:])
 
-        for file in os.listdir(rf'{path}\labels'):
+        for file in os.listdir(rf'{path}/labels'):
             if file.endswith('.tif'):
-                labs.append(to_sparse(tifffile.imread(rf'{path}\labels\{file}')))
+                labs.append(to_sparse(tifffile.imread(rf'{path}/labels/{file}')))
 
     elif part == 'train':
-        for file in os.listdir(rf'{path}\images'):
+        for file in os.listdir(rf'{path}/images'):
             if file.endswith('.npy'):
-                ims.append(np.moveaxis(np.load(rf'{path}\images\{file}'), -1, 0)[:,:,:]/24_000)
+                ims.append(np.load(rf'{path}/images/{file}')[:,:,:]/24_000)
 
-        for file in os.listdir(rf'{path}\labels'):
+        for file in os.listdir(rf'{path}/labels'):
             if file.endswith('.npy'):
-                labs.append(to_sparse(np.load(rf'{path}\labels\{file}')[0]))
+                labs.append(to_sparse(np.load(rf'{path}/labels/{file}')[0]))
 
-    ims = np.asarray(ims)
+    ims = np.moveaxis(np.asarray(ims), 1, -1)
     labs = np.asarray(labs)
 
     return ims, labs
@@ -42,15 +42,15 @@ def load_data(path, part):
 def get_file_names(path):
 
     ims, labs = [], []
-    for file in os.listdir(rf'{path}\images'):
+    for file in os.listdir(rf'{path}/images'):
         if file.endswith('.npy'):
-            ims.append(rf'{path}\images\{file}')
+            ims.append(rf'{path}/images/{file}')
 
-    for file in os.listdir(rf'{path}\labels'):
+    for file in os.listdir(rf'{path}/labels'):
         if file.endswith('.npy'):
-            labs.append(rf'{path}\labels\{file}')
+            labs.append(rf'{path}/labels/{file}')
 
-    return ims, labs
+    return np.asarray(ims), np.asarray(labs)
 
 def shuffle(im, lab):
     p = np.random.permutation(len(im))
