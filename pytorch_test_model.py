198ce36f77077f8a10156e7e259dd6422d532541
meh0w <michal.gruba00@gmail.com>
2024-09-28 12:59:16 +0200

    Before new IoU calculations

--- pytorch_test_model.py
+++ pytorch_test_model.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from pytorch_MU_NET import MUNet as MU_Net
 import pytorch_MU_NET_quant as experimental
 from pytorch_metrics import All_metrics
-from pytorch_generator import DataLoaderSNOWED, DataLoaderSWED, DataLoaderSWED_NDWI,DataLoaderSWED_NDWI_np
+from pytorch_metrics_new import All_metrics2
+from pytorch_generator import DataLoaderSNOWED, DataLoaderSWED, DataLoaderSWED_NDWI,DataLoaderSWED_NDWI_np, DataLoaderSNOWED_NDWI
 import os
 import pprint
 from tifffile import tifffile
@@ -12,35 +13,67 @@ import numpy as np
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from time import time
+import torch.nn.utils.prune as prune
+from fasterai.misc.bn_folding import BN_Folder
 
-def quantize(model,device):
-    model.qconfig = torch.ao.quantization.get_default_qconfig('x86')
-    # model = torch.ao.quantization.fuse_modules(model, [['conv', 'relu']])
-    model_fp32_prepared = torch.ao.quantization.prepare(model)
-    img_files, label_files = get_file_names(rf'.\SWED\train', '.npy', 'SWED_FULL')
-    idx = np.random.choice(np.arange(len(img_files)), int(np.floor(len(img_files)*0.7)), replace=False)
-    data_set_valid = DataLoaderSWED_NDWI_np(np.delete(img_files, idx), np.delete(label_files, idx), False, precision=32)
-    data_loader_valid = DataLoader(data_set_valid, batch_size=10, shuffle=True, pin_memory=True, num_workers=2)
-    for i, data in enumerate(data_loader_valid):
-        img, label = data
-        model_fp32_prepared(img.to(device))
-        break
-    model_int8 = torch.ao.quantization.convert(model_fp32_prepared.to('cpu'))
-    model_int8 = model_int8.to(device)
-
-    return model_int8
+def print_size_of_model(model):
+    torch.save(model.state_dict(), "temp.pt")
+    print()
+    size = f"Size (MB): {os.path.getsize('temp.pt')/1e6}"
+    os.remove('temp.pt')
+    return size
 
 
-def test(data_path, weights_path, data_loader, dataset, m, display, save, result_path, device='cpu'):
+def test(data_loader, dataset, m, display, save, result_path, options, device='cpu'):
     m.eval()
-    if QUANT:
-        # m = quantize(m,device)
+
+    if options['bn fold']:
+        bn = BN_Folder()
+        m = bn.fold(m)
+        print('BN FOLDED')
+
+    if options['prune']:
+        parameters_to_prune = (
+        (m.cnn_encoder.down1.doubleconv.double_conv[0], 'weight'),
+        (m.cnn_encoder.down1.doubleconv.double_conv[3], 'weight'),
+        (m.cnn_encoder.down2.doubleconv.double_conv[0], 'weight'),
+        (m.cnn_encoder.down2.doubleconv.double_conv[3], 'weight'),
+        (m.cnn_encoder.down3.conv[0], 'weight'),
+        (m.cnn_encoder.down4.conv[0], 'weight'),
+        (m.trans_decoder.up1.conv[0], 'weight'),
+        (m.trans_decoder.up2.conv.double_conv[0], 'weight'),
+        (m.trans_decoder.up2.conv.double_conv[3], 'weight'),
+        (m.trans_decoder.up3.conv.double_conv[0], 'weight'),
+        (m.trans_decoder.up3.conv.double_conv[3], 'weight'),
+        (m.trans_decoder.up4.conv.double_conv[0], 'weight'),
+        (m.trans_decoder.up4.conv.double_conv[3], 'weight'),
+        )
+
+        prune.global_unstructured(
+        parameters_to_prune,
+        pruning_method=prune.L1Unstructured,
+        amount=options['prune amount'],
+        )
+        
+        for param in parameters_to_prune:
+            prune.remove(param[0], 'weight')
+        print('PRUNED')
+
+    with open(rf'{result_path}.txt', 'w') as f:
+        f.write(f'Total params: {sum(p.numel() for p in m.parameters())} \n')
+        f.write(f'Trainable params: {sum(p.numel() for p in m.parameters() if p.requires_grad)} \n')
+
+    if options['quant']:
         m = torch.ao.quantization.quantize_dynamic(
             m,  # the original model
             {torch.nn.Linear, torch.nn.Conv2d},  # a set of layers to dynamically quantize
-            dtype=torch.qint8)
+            dtype=options['quant dtype'])
+        print('QUANTIZED')
+
+
 
     test_metrics = All_metrics(device, '[TEST]')
+    test_metrics2 = All_metrics2(device, '[TEST]')
     softmax = torch.nn.Softmax(dim=1)
     val_epoch_progress = tqdm(
         data_loader, f"[EVAL] (TEST SET)", leave=False
@@ -76,24 +109,31 @@ def test(data_path, weights_path, data_loader, dataset, m, display, save, result
             *_, last1, last2,last3 = data_eval
             if save:
                 metrics = test_metrics.get(True)
-                with open(rf'{result_path}.txt', 'w') as f:
+                with open(rf'{result_path}.txt', 'a') as f:
                     for key, value in metrics.items():
                         print(f'{key}: {np.round(value, 4):.4f} \n')
                         f.write(f'{key}: {np.round(value, 4):.4f} \n')
+                    f.write(f'Inference time: {np.mean(times[1:])}\n')
+                    f.write(f'{print_size_of_model(m)}')
+                    f.write(f'Optimization options: {options} \n')
+
             else:
                 metrics = test_metrics.get(True)
                 for key, value in metrics.items():
                     print(f'{key}: {np.round(value, 4):.4f} \n')
             print('a')
 
-    number_of_rows = min(6, len(val_epoch_progress))
-    fig, ax = plt.subplots(number_of_rows, 3)
+    no_predictions = True
+
+    number_of_rows = 4 if no_predictions else min(6, len(val_epoch_progress))
+    number_of_cols = 2 if no_predictions else 3
+    fig, ax = plt.subplots(number_of_rows, number_of_cols, figsize=(number_of_cols,number_of_rows))
     j = 0
     for entry in [data_eval[0],data_eval[1],data_eval[2], last1, last2,last3]:
         i = entry['idx']
         print(entry['iou'])
     # for i in range(len(predicted)):
-        if isinstance(dataset, DataLoaderSWED):
+        if isinstance(dataset, DataLoaderSWED) or isinstance(dataset, DataLoaderSNOWED_NDWI):
             rgb = np.moveaxis(dataset[i][0].numpy(), 0, -1)[:,:,[0,1,2]]
             real = np.moveaxis(dataset[i][1].numpy(), 0, -1)
         else:
@@ -103,7 +143,17 @@ def test(data_path, weights_path, data_loader, dataset, m, display, save, result
         # clipped = rgb
         with torch.no_grad():
             predicted = softmax(m(dataset[i][0].to(device).unsqueeze(0)))
-        if number_of_rows > 1:
+
+        if no_predictions:
+            ax[j, 0].imshow(clipped)
+            ax[j, 1].imshow(np.argmax(real,axis=-1))
+
+            ax[j, 0].set_xticks([])
+            ax[j, 0].set_yticks([])
+
+            ax[j, 1].set_xticks([])
+            ax[j, 1].set_yticks([])
+        elif number_of_rows > 1:
 
             # clipped = rgb
             # ax[j, 0].hist(rgb[:,:,0].flatten(), bins=100)
@@ -139,7 +189,16 @@ def test(data_path, weights_path, data_loader, dataset, m, display, save, result
             ax[2].set_yticks([])
 
         j += 1
-        if j == number_of_rows:
+        if no_predictions and j == number_of_rows:
+            plt.subplots_adjust(0, 0, 1, 1, 0, 0)
+            if save:
+                # fig.set_size_inches(2.4,4.75)
+                plt.savefig(f'{result_path}.jpg', dpi=600)
+            if display:
+                plt.show()
+            fig, ax = plt.subplots(5, 3)
+            j = 0
+        elif j == number_of_rows:
             # plt.subplots_adjust(0.021, 0.012, 0.376, 0.998, 0, 0.067)
             plt.subplots_adjust(0, 0, 1, 1, 0, 0)
             if save:
@@ -151,11 +210,11 @@ def test(data_path, weights_path, data_loader, dataset, m, display, save, result
             j = 0
         print(j)
 
-def test_one_folder(folder, result_folder=None):
+def test_one_folder(folder, opts, result_folder=None):
     BATCH_SIZE = 1
     weights = rf'.\{folder}\BEST.pt'
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    if QUANT:
+    device = opts['device']
+    if opts['quant']:
         device = 'cpu'
 
     if 'DeepUNet' in folder:
@@ -168,8 +227,8 @@ def test_one_folder(folder, result_folder=None):
         m = SeNet2.get_model(data_loader_train.input_size, BATCH_SIZE)
     elif 'MU_Net' in folder:
         # m = MU_Net([32,64,128,256], base_c=32, bilinear=False)
-        m = MU_Net([4,64,128,256,512]) #1st NDWI
-        # m = MU_Net([4, 32,64,128,256], base_c=32)
+        # m = MU_Net([4,64,128,256,512]) #1st NDWI #OSIOSN1
+        m = MU_Net([4, 32,64,128,256], base_c=32) #OSIOSN2
         # m = experimental.MUNet([4, 32,64,128,256], base_c=32)
 
         # m = MU_Net()
@@ -189,7 +248,8 @@ def test_one_folder(folder, result_folder=None):
     elif 'SNOWED' in folder:
         PATH = rf'.\SNOWED_TEST'
         img_files = get_file_names(PATH, '.npy', 'SNOWED')
-        data_set_test = DataLoaderSNOWED(img_files, False)
+        # data_set_test = DataLoaderSNOWED(img_files, False)
+        data_set_test = DataLoaderSNOWED_NDWI(img_files, False, root_path=PATH)
 
     elif 'SWED_FULL' in folder:
         PATH = rf'.\SWED\test'
@@ -198,23 +258,35 @@ def test_one_folder(folder, result_folder=None):
 
     loader = DataLoader(data_set_test, batch_size=BATCH_SIZE, shuffle=False)
 
-    save = False
+    save = True
     display = True
     if save and result_folder is None:
         raise Exception('Results folder not specified')
     else:
         file = folder.split('\\')[-1]
         result_path = rf"{result_folder}\{file}"
-        test(PATH, weights, loader, data_set_test, m, display, save, result_path, device)
+        test(loader, data_set_test, m, display, save, result_path, opts, device)
 
 
 if __name__ == '__main__':
     base = rf'weights\MU_Net\REPORT 20-04-2024'
-    res = rf'plots\OSIOSN'
-    os.makedirs(res, exist_ok=True)
-    QUANT = True
+
+    for d in ['cuda']:
+        res = rf'plots\Magisterka\przyklad_dane'
+        os.makedirs(res, exist_ok=True)
+        optimize_options = {
+            'quant': False,
+            'prune': False,
+            'bn fold': False,
+            'quant dtype': torch.qint8,
+            'prune amount': 0.3,
+            'device': d
+        }
+    # QUANT = False
+    # PRUNE = True
+    # BN_FOLD = False
     # folder = rf'weights\MU_Net\2024-04-12 01_00_48 Dice+Crossentropy SWED_FULL 1e-03 sample' #1)?
-    folder = rf'weights\MU_Net\2024-04-14 20_18_09 Dice+Crossentropy SWED_FULL 1e-03 sample' #2)? #OSIOSN #1
+    # folder = rf'weights\MU_Net\2024-04-14 20_18_09 Dice+Crossentropy SWED_FULL 1e-03 sample' #2)? #OSIOSN #1
     # folder = rf'weights\MU_Net\2024-04-16 02_28_26 Dice+Crossentropy SWED_FULL 1e-03 sample' #3)?
     # folder = rf'weights\MU_Net\2024-04-16 12_50_21 Dice+Crossentropy SWED_FULL 1e-03 sample' #4)?
     # folder = rf'weights\MU_Net\2024-05-25 13_45_24 Dice+Crossentropy SWED 1e-03 sample'
@@ -224,7 +296,9 @@ if __name__ == '__main__':
     # folder = rf'weights\MU_Net\2024-04-14 17_55_05 Dice+Crossentropy SWED 1e-03 sample'
 
     # BEST FULL SWED
-    # folder = rf'weights\MU_Net\2024-04-16 02_28_26 Dice+Crossentropy SWED_FULL 1e-03 sample' #OSIOSN #2
+        folder = rf'weights\MU_Net\2024-04-16 02_28_26 Dice+Crossentropy SWED_FULL 1e-03 sample' #OSIOSN #2 smaller
+
+    # folder = rf'weights\MU_Net\2024-04-12 01_00_48 Dice+Crossentropy SWED_FULL 1e-03 sample' #SGD
 
     # folder = rf'weights\MU_Net\2024-05-27 01_38_49 Dice+Crossentropy SWED_FULL 1e-03 sample'
 
@@ -233,6 +307,12 @@ if __name__ == '__main__':
     # no split
     # folder = rf'weights\MU_Net\2024-05-29 01_55_09 Dice+Crossentropy SWED_FULL 1e-03 sample'
 
-    test_one_folder(folder, res)
-    # for folder in os.listdir(base):
-    #     test_one_folder(rf'{base}\{folder}', res)
+    #OG
+    # folder = rf'weights\MU_Net\2024-09-08 18_16_56 Dice+Crossentropy SWED_FULL 1e-03 sample'
+
+    #SGD
+        # folder = rf'weights\MU_Net\2024-09-10 03_21_11 Dice+Crossentropy SWED_FULL 1e-03 sample'
+
+        test_one_folder(folder, optimize_options, res)
+        # for folder in os.listdir(base):
+        #     test_one_folder(rf'{base}\{folder}', res)
